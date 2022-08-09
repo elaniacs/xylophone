ef47acca2ae7ce3d75bfc963ce3f808cc4099d8a
elaniacs <carensousaesilva@gmail.com>
2022-08-09 19:02:29 -0300

    add storyboard, sounds and launchScreen

---
+++ xylophone/xylophone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xylophone
+//
+//  Created by Cáren Sousa on 09/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
